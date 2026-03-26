f30e191b76067eaba59740a8b5ea16c56ed69c34
vasanth aggala <vasanth.aggala@gmail.com>
2026-03-26 00:57:49 -0400

    test; just changed text and icon

--- StudyBuddies/ContentView.swift
+++ StudyBuddies/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "book.fill")
                 .imageScale(.large)
+                .foregroundStyle(.black)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("StudyBuddies :)")
         }
         .padding()
     }
